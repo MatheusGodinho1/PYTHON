6dccafc7aea814d31d6be340237506261b060c8b
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-10-22 17:33:28 -0300

    Create como abrir os forms com python.py

---
+++ QT Designer/como abrir os forms com python.py
@@ -0,0 +1,16 @@
+pyuic5 arquivo.ui -o arquivo.py
+
+
+
+
+Codigo que vai no final do arquivo ''py'' gerado. responsavel por ler o grafico e chamar:
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
